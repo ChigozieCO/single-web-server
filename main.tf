e03e33fb0d7a694e565f022fc00c78b63900be4f
TheGozie <chiggy.nm@gmail.com>
2023-01-02 16:55:20 +0100

    Added Terraform Configuration files and made format changes to main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 # Configure a single EC2 Instance. 
 # Because we have not specified any VPC ID, this instance would be deployed in the default VPC with the subnet chosen at random.
 
 resource "aws_instance" "server1" {
-    ami = "ami-0a6b2839d44d781b2"
-    instance_type = "t2.mirco"
+  ami           = "ami-0a6b2839d44d781b2"
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "tf-server1"
-    }
+  tags = {
+    Name = "tf-server1"
+  }
 }
